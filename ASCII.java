b1619f284930bc2a89e6fb9d5367ddca0ccb4548
jayshreee10 <2001109280_ee@pmec.ac.in>
2025-12-27 20:45:00 +0530

    Ascii

--- ASCII.java
+++ ASCII.java
@@ -8,8 +8,12 @@ public class ASCII {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number");
         int value = sc.nextInt();
-        char ch = (char) value;
-        System.out.println(ch);
+        if (value >= 65 && value <= 122) {
+            char ch = (char) value;
+            System.out.println(ch);
+        } else
+            System.out.println("Value must in between 65 to 122");
+
         sc.close();
     }
 }
