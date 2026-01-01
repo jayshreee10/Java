494e47a4aecfc683693936a0f3b4ea9667be820f
jayshreee10 <2001109280_ee@pmec.ac.in>
2026-01-01 15:20:41 +0530

    fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,10 +7,15 @@ public class Fibonacci {
     public static void main(String args[]) {
         int limit = 10;
         int sum = 0;
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(n1 + ",");
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.print(i + ",");
-
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
         }
     }
 }
