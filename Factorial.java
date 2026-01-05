94c3342f08be12feb1a2f954dc9870d5921a9a19
jayshreee10 <2001109280_ee@pmec.ac.in>
2026-01-05 23:16:04 +0530

    factorial

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,18 @@
 package programs;
 
 //Factorial of a number
+// Input: n = 5
+// Output: 120
+// Explanation: 5! = 5 * 4 * 3 * 2 * 1 = 120
 public class Factorial {
+    public static void main(String args[]) {
+        int input = 5;
+        int value = 1;
 
+        for (int i = 1; i <= input; i++) {
+            value = value * i;
+        }
+
+        System.out.println(value);
+    }
 }
